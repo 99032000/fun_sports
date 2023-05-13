bd3c035a33b411bbdccd3c080cfeadbb54c81976
damon_gege <z3439119@gmail.com>
2023-05-13 22:25:28 +1000

    add new hobbies when signup

---
+++ components/signUp/SportsLevelSelector.tsx
@@ -0,0 +1,58 @@
+"use client";
+import { Dispatch, SetStateAction } from "react";
+import type { hobby_type } from "./SignUp";
+import { SportsLevelName } from "@/utility/Data_defination";
+import { AiFillDelete } from "react-icons/ai";
+const SportsLevel = ({
+  hobby,
+  setHobbyList,
+}: {
+  hobby: hobby_type;
+  setHobbyList: Dispatch<SetStateAction<hobby_type[]>>;
+}) => {
+  const handleDelete = () => {
+    setHobbyList((pre) => {
+      return pre.filter((item) => item.sports_typeId !== hobby.sports_typeId);
+    });
+  };
+  const handleSelect = (e: any) => {
+    setHobbyList((pre) => {
+      return pre.map((item) => {
+        if (item.sports_typeId === hobby.sports_typeId) {
+          return {
+            ...item,
+            level: e.target.value,
+          };
+        }
+        return item;
+      });
+    });
+  };
+  return (
+    <div className=" my-4 flex flex-row  max-w-full justify-between">
+      <div className=" flex flex-row gap-4 flex-1">
+        <p className=" w-fit">{hobby.sports_name}</p>
+        <select
+          className="select md:select-sm w-full max-w-[150px] sm:max-w-[300px] md:max-w-[350px] select-primary select-xs"
+          defaultValue={hobby.level}
+          onChange={handleSelect}
+        >
+          <option disabled value={-1}>
+            Choose a level
+          </option>
+          {SportsLevelName.map((sport, index) => (
+            <option key={index} value={index}>
+              {sport}
+            </option>
+          ))}
+        </select>
+      </div>
+      <AiFillDelete
+        className=" text-secondary w-6 h-6 cursor-pointer hover:text-secondary-focus"
+        onClick={handleDelete}
+      />
+    </div>
+  );
+};
+
+export default SportsLevel;
---
+++ utility/Data_defination.ts
@@ -0,0 +1,13 @@
+
+
+export const SportsLevelName: string[] = [
+  "GrandMaster",
+  "Master",
+  "High",
+  "Medium",
+  "Noob",
+  "卧龙凤雏",
+  "黑洞",
+  "rex 专属 夕阳红无敌老菜逼",
+  "damon图书馆💻 无敌段位 没有之一"];
+
