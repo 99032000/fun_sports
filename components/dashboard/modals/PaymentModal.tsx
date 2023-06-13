2dc495f8c87f63fb1131f1dddf3aa1051f0f52e2
damon_gege <z3439119@gmail.com>
2023-06-13 22:06:19 +1000

    clean code

--- components/dashboard/modals/PaymentModal.tsx
+++ components/dashboard/modals/PaymentModal.tsx
@@ -5,16 +5,10 @@ function PaymentModal({ socialBooking }: { socialBooking: social_booking }) {
   return (
     <dialog id={`paymentModal${socialBooking.id}`} className="modal">
       <form method="dialog" className="modal-box">
-        <button
-          htmlFor={`paymentModal${socialBooking.id}`}
-          className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
-        >
+        <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
           ✕
         </button>
         <Payment socialBooking={socialBooking} />
-        <div className="modal-action">
-          {/* if there is a button in form, it will close the modal */}
-        </div>
       </form>
     </dialog>
   );
