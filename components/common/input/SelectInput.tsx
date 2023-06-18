8659ad05676d1f9de3db282ab456149b6ffa352c
norriswu0 <norris.wu.au+github@outlook.com>
2023-06-19 00:20:41 +1000

    add select input with controlled variant

---
+++ components/common/input/SelectInput.tsx
@@ -0,0 +1,61 @@
+import { SelectHTMLAttributes } from "react";
+import { Control, useController } from "react-hook-form";
+
+type SelectOption = {
+  id: number;
+  name: string;
+};
+type SelectInputProps = {
+  required?: boolean;
+  label: string;
+  options: SelectOption[];
+} & SelectHTMLAttributes<HTMLSelectElement>;
+export function SelectInput({
+  required,
+  name,
+  label,
+  placeholder,
+  options,
+  ...props
+}: SelectInputProps) {
+  const _label = `${label}:${required ? "*" : ""}`;
+
+  const _options = [{ id: -1, name: placeholder }, ...options];
+
+  return (
+    <div className="flex flex-row gap-4 mt-8 flex-wrap">
+      <label htmlFor={name} className=" my-auto text-sm sm:text-lg">
+        {_label}
+      </label>
+      <select
+        id={name}
+        className="select max-w-fit w-full sm:max-w-[300px] md:max-w-[350px] select-primary select-sm text-xs sm:text-sm"
+        defaultValue={-1}
+        {...props}
+      >
+        {_options.map(({ id, name }) => (
+          <option key={id} value={id}>
+            {name}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
+
+type ControlledSelectInputProps = {
+  name: string;
+  control: Control<any>;
+} & SelectInputProps;
+export function ControlledSelectInput({
+  name,
+  control,
+  ...props
+}: ControlledSelectInputProps) {
+  const { field } = useController({
+    name,
+    control,
+  });
+
+  return <SelectInput {...field} {...props} />;
+}
